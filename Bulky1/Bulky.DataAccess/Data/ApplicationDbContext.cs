222d4148b6c7f4d169bcec36a98eaf6b80342276
AbdallahDaradke <103438260+AbdallahDaradke@users.noreply.github.com>
2025-05-12 15:47:01 +0300

    Add DataAccess folder from bulky1 project
    I am adding them component by component because I cannot add them all together at once

---
+++ Bulky1/Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,64 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        // base(options): like that we pass configurations to the base class of DbContext(Review the course
+        //Lecture for more details)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Category> Categories { get; set; }//By this line, create this table automatically
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }//By this line, create this table automatically
+
+        public DbSet<Product> Products { get; set; }//By this line, create this table automatically
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);//We need the base.OnModelCreating line so the program can mapped the primary key if not it will show error
+            //Here, we are seeding the table, The table is already created
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Active", DisplayOrder = 4 },
+                new Category { Id = 3, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 4, Name = "History", DisplayOrder = 3 }
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "FOC",
+                    Description = "Fundamentals Of Computing",
+                    ISBN = "1234567890",
+                    Author = "Abdallah Daradkeh",
+                    ListPrice = 20.99,
+                    Price50 = 18.99,
+                    Price100 = 16.99,
+                    CategoryId = 1,
+                    ImageUrl=""
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Programming",
+                    Description = "Fundamentals Of Programming Using Java",
+                    ISBN = "1244567890",
+                    Author = "Mohammad Daradkeh",
+                    ListPrice = 23.99,
+                    Price50 = 22.99,
+                    Price100 = 20.99,
+                    CategoryId = 2,
+                    ImageUrl = ""
+                }
+                );
+        }
+    }
+}
---
+++ Bulky1/Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class//Internal is equivelant I think to access modifier such as public
+    {
+        //T - Category
+        IEnumerable<T> GetAll();//Get all
+        T Get(Expression<Func<T,bool>> filter);//Get single
+
+        void Add(T entity);//Add
+        void Remove(T entity);//Remove
+        void RemoveRange(IEnumerable<T> entity);//Remove range
+
+        //We don't have update and save methods because they are complicated, and it is not preferable
+        //to have it inide The Generic Repository
+    }
+
+}
